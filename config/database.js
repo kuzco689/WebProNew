67a3f535bd9db03d67eea5f5bb2e9bfb19bc4bdf
kuzco689 <kuzco689@gmail.com>
2019-05-30 13:21:14 +0700

    heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database:'mongodb://localhost:27017/ipa_fsci',
+  database:'mongodb+srv://kuzco689:1234@cluster0-e8x42.mongodb.net/ipa_fsci?retryWrites=true&w=majority',
   secret: 'yoursecret'
 }
